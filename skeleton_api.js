cca616731dcf03cbe6fb847c56dc4d8c513c4435
teddy@dnc-algo.com <lazebnik.teddy@gmail.com>
2020-09-09 10:03:20 +0300

    api calls are almost ready

--- skeleton_api.js
+++ skeleton_api.js
@@ -1,7 +1,6 @@
 let urlAnswer = "";
 let ERROR_STRING = "e";
 let API_URL = "https://basket.roundu.ai/api/process";
-let API_RESULT_URL = "https://basket.roundu.ai/result_url";
 
 let sekeletons_gt = [];
 let sekeletons_sample = [];
@@ -10,6 +9,8 @@ async function make_api_call()
 {
 	try
 	{
+		document.getElementById("start_btn").style.display = "none";
+		
 		// get needed data 
 		var gt_url = document.getElementById("gt").value;
 		var sample_url = document.getElementById("sample").value;
@@ -24,13 +25,14 @@ async function make_api_call()
 		{
 			throw "HTTP call failed";
 		}
-		var gt_url_path = urlAnswer;
+		var gt_url_path = urlAnswer["result_url"];
+		urlAnswer = "";
 		
 		// wait until answer is ready
 		var flagAnswerIsFine = false;
 		while(!flagAnswerIsFine)
 		{		
-			make_http_call(API_RESULT_URL, "get", gt_url_path);
+			make_http_call(gt_url_path, "get");
 			while (urlAnswer == "")
 			{
 				await sleep(100);
@@ -39,11 +41,11 @@ async function make_api_call()
 			{
 				throw "HTTP call failed";
 			}
-			var answer = Json.parse(urlAnswer);
+			var answer = urlAnswer;
+			urlAnswer = "";
 			if (answer["processed"])
 			{
-				sekeletons_gt = downloadSkeletons(answer["skeleton_url"]);
-				flagAnswerIsFine = true;
+				await downloadSkeletons(answer["skeleton_url"], true);
 			}
 		}
 		
@@ -56,12 +58,13 @@ async function make_api_call()
 		{
 			throw "HTTP call failed";
 		}
-		var sample_url_path = urlAnswer;
+		var sample_url_path = urlAnswer["result_url"];
+		urlAnswer = "";
 		
 		var flagAnswerIsFine = false;
 		while(!flagAnswerIsFine)
 		{		
-			make_http_call(API_RESULT_URL, "get", sample_url_path);
+			make_http_call(sample_url_path, "get");
 			while (urlAnswer == "")
 			{
 				await sleep(100);
@@ -70,11 +73,11 @@ async function make_api_call()
 			{
 				throw "HTTP call failed";
 			}
-			var answer = Json.parse(urlAnswer);
+			var answer = urlAnswer;
+			urlAnswer = "";
 			if (answer["processed"])
 			{
-				sekeletons_sample = downloadSkeletons(answer["skeleton_url"]);
-				flagAnswerIsFine = true;
+				await downloadSkeletons(answer["skeleton_url"], false);
 			}
 		}
 		
@@ -90,23 +93,25 @@ async function make_api_call()
 	catch (error)
 	{
 		alert("Error in your request, saying: " + error);
+		document.getElementById("start_btn").style.display = "";
 	}
 }
 
-async function make_http_call(url, method, value)
+async function make_http_call(url, method, value = "")
 {
 	try
 	{
 		httpAnswer = "";
 		const xhr = new XMLHttpRequest();
-		xhr.open(method, url);
-		xhr.setRequestHeader('Content-Type', 'application/json');
 		if (method == "post")
 		{
+			xhr.open(method, url, true);
+			xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 			xhr.send("video=" + value);
 		}
 		else // method == "get"
 		{
+			xhr.open(method, url);
 			xhr.send();
 		}
 
@@ -114,7 +119,7 @@ async function make_http_call(url, method, value)
 		{
 			if (this.readyState == 4 && this.status == 200)
 			{
-				urlAnswer =  this.responseText;
+				urlAnswer = JSON.parse(this.responseText);
 			}
 			else
 			{
@@ -124,13 +129,56 @@ async function make_http_call(url, method, value)
 	}
 	catch (error)
 	{
-		console.log("Error");
+		console.log("Error in 'make_http_call' saying: " + error);
 	}
 }
 
-async function downloadSkeletons()
+async function downloadSkeletons(skeleton_url, is_gt)
 {
+	try
+	{
+		make_http_call(skeleton_url, "get");
+		while (urlAnswer == "")
+		{
+			await sleep(100);
+		}
+		if (urlAnswer == ERROR_STRING)
+		{
+			throw "HTTP call failed";
+		}
+		var data = urlAnswer;
+		urlAnswer = "";
+	}
+	catch (error)
+	{
+		console.log("Error in 'downloadSkeletons' saying: " + error);
+	}
 	
+	var frameSkeleton = [];
+	for (var itemIndex = 0; itemIndex < data.length; itemIndex++)
+	{
+		frameSkeleton.push([data[itemIndex]["frame"], data[itemIndex]["people"][0]["pose_keypoints_2d"]]);
+	}
+	frameSkeleton.sort(function(a, b)
+	{ 
+		return parseInt(a[0]) - parseInt(b[0]);
+	});
+	
+	var answer = [];
+	for (var itemIndex = 0; itemIndex < data.length; itemIndex++)
+	{
+		answer.push(frameSkeleton[itemIndex][1]);
+	}
+	
+	if (is_gt)
+	{
+		sekeletons_gt = answer;
+	}
+	else
+	{
+		sekeletons_sample = answer;
+	}
+	flagAnswerIsFine = true;
 }
 
 function sleep(ms) {
